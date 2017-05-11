90b844459a32ba813ece5db7067a35ce029a9c6e
JapSeyz <JapSeyz@JapSeyz.com>
2017-05-11 11:26:45 +0200

    Init
    Master code

---
+++ Spoiler.ino
@@ -0,0 +1,38 @@
+/*
+ * Include our own Libraries
+ */
+#include <Parser.h> // Connects to and speaks with the CAN bus
+#include <Spoiler.h> // Moves the Spoiler
+
+int forwardsPin = 5;
+int backwardsPin = 6;
+int resistorPin = A0;
+
+/**
+ * Initiate Libraries
+ */
+Parser parser;
+Spoiler spoiler(forwardsPin, backwardsPin, resistorPin);
+
+void setup() {
+  spoiler.POT_MIN = 100;
+  spoiler.POT_MAX = 600;
+  
+  //parser.init();
+  spoiler.init();
+
+  Serial.begin(9600);
+}
+
+void loop() {
+    //parser.listen();
+
+    //int angle = map(parser.speed,0, 75, 100, 600);
+    //Serial.println(angle);
+
+    int angle = random(100, 600);
+    Serial.println(angle);
+
+    spoiler.setPosition(angle);
+    delay(300);
+}
